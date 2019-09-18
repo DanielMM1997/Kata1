8acc30a2ac781eaab63f2e67b519d144b3f8b6ff
Usuario <Usuario@10.22.148.96>
2019-09-18 12:02:45 +0100

    Finished

---
+++ src/kata1/Person.java
@@ -0,0 +1,24 @@
+package kata1;
+
+import java.util.Date;
+
+public class Person {
+
+    private final String name;
+    private final Date birthday;
+
+    public Person(String name, Date birthday) {
+        this.name = name;
+        this.birthday = birthday;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+    
+    
+}
