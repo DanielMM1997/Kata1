598ef747286a1e6c1940f86cdf946a96b5d89d97
danie <danie@LAPTOP-1KLEHBU3>
2019-09-19 00:29:30 +0100

    version 2

--- src/kata1/Main.java
+++ src/kata1/Main.java
@@ -1,9 +1,17 @@
 package kata1;
 
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
 public class Main {
 
     public static void main(String[] args) {
-        // TODO code application logic here
+        Calendar date = GregorianCalendar.getInstance();
+        date.set(2000, 1, 24);
+
+        Person person = new Person("Juan", date);
+        System.out.println(person.getName() + " tiene " + person.getAge() 
+                            + " años.");
     }
     
 }
--- src/kata1/Person.java
+++ src/kata1/Person.java
@@ -1,13 +1,16 @@
 package kata1;
 
-import java.util.Date;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
 
 public class Person {
 
     private final String name;
-    private final Date birthday;
+    private final Calendar birthday;
+    private final long MILLISECONDS_PER_YERAS = (long) (1000*60*60*24*365.25);
 
-    public Person(String name, Date birthday) {
+    public Person(String name, Calendar birthday) {
         this.name = name;
         this.birthday = birthday;
     }
@@ -16,8 +19,17 @@ public class Person {
         return name;
     }
 
-    public Date getBirthday() {
+    public Calendar getBirthday() {
         return birthday;
     }
-
+    
+    public int getAge(){
+        Calendar today = GregorianCalendar.getInstance();
+        return (int) (milisecondsToYears(today.getTimeInMillis() - 
+                        birthday.getTimeInMillis()));
+    }
+    
+    private long milisecondsToYears(long millis){
+        return millis/MILLISECONDS_PER_YERAS;
+    }
 }
